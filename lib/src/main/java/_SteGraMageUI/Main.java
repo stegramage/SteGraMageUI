2c81747b63294955ab8ab28c9c8aa11c9cf95185
Daniel Carrillo <carrillo.daniel2019@gmail.com>
2024-05-26 16:03:21 -0300

    cambio Main Interprete -> Codec

--- lib/src/main/java/_SteGraMageUI/Main.java
+++ lib/src/main/java/_SteGraMageUI/Main.java
@@ -7,7 +7,7 @@ public class Main {
 	public static void main(String[] args) {
 		
 		SteGraMage model = new SteGraMage();
-		model.configure(new MokingConverter(), new ASCIIMessageInterpreter());
+		model.configure(new MokingConverter(), new ASCIIMessageCodec());
 
 		SteGraMageUI view = new SteGraMageUI("TXT");
 		
